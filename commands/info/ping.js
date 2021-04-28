714f908e4a5ecedf2b590ef670e10e3c5556ef29
HeldEnderMen <62526125+HeldEnderMen@users.noreply.github.com>
2021-04-28 12:07:06 -0400

    Create ping.js

---
+++ commands/info/ping.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.json");
+
+module.exports = {
+  name: "ping",
+  aliases: [],
+  description: "Pong!",
+  usage: "Ping",
+  run: async (client, message, args) => {
+    const embed = new MessageEmbed()
+      .setColor(Color)
+      .setDescription(`Pong - ${client.ws.ping}ms`)
+      .setFooter(`Requested By ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed).catch(error =>
+    message.reply(
+      `Sorry but I couldn't run work because of: ${error}`
+    ));
+
+    //End
+  }
+};
